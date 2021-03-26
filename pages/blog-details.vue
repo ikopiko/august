8f2609efc6712249cd01484c82c11affbceb63c3
webertela <59246555+ubuntu-new@users.noreply.github.com>
2021-03-26 20:00:02 +0400

    Uploaded From LOXA PC

---
+++ pages/blog-details.vue
@@ -0,0 +1,104 @@
+<template>
+<div>
+    <!-- <div class="page-title-area White">
+        <div class="container">
+            <ul>
+                <li>
+                    <nuxt-link to="/">Home</nuxt-link>
+                </li>
+                <li>Styling White Jeans after Labor Day</li>
+            </ul>
+        </div>
+    </div> -->
+
+    <section class="blog-details-area mt-90">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="blog-details">
+                        <div class="article-img">
+                            <img src="../assets/img/blog-main-detail.jpg" alt="image">
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row  mt-50">
+                <div class="col-md-2">&nbsp;</div>
+                <div class="col-md-8">
+                    <div class="article-content">
+
+                        <h3 class="text-center title-detail mb-85">Why just add one? Because it’s achievable. If you add more than that Cool Pastry</h3>
+                    </div>
+                </div>
+                <div class="col-md-2">2</div>
+            </div>
+        </div>
+
+        <div class="container-fluid fullBg">
+            <div class="row">
+                <div class="col-md-12  mt-75 mb-75">
+
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-md-2">
+                                <div class="share text-center">
+                                    <p>Share</p>
+                                    <p class="xazi">
+                                        &nbsp;
+                                    </p>
+                                    <ul class="shareUl">
+                                        <li class="shareLi"><img src="../assets/img/facebook.svg" class="facebook"></li>
+                                        <li class="shareLi"><img src="../assets/img/linkedin-1.svg" class="facebook"></li>
+                                        <li class="shareLi"><img src="../assets/img/path.svg" class="facebook"></li>
+                                    </ul>
+                                </div>
+                            </div>
+                            <div class="col-md-8 ">
+                                <div class="blog-details">
+
+                                    <ul class="entry-meta" style="padding-bottom:30px">
+                                        <li class="mr-40"><img src="../assets/img/book-04.svg" class="Book-04 "> 5 Min Read</li>
+                                        <li>April 08, 2019</li>
+                                    </ul>
+
+                                    <p>Through solution-based therapy, I learned about a pair of questions that can help you identify exactly what you need to do to move forward each day. I’ve been asking myself these questions every night, right before bed — research shows that our brains can make creative connections while we sleep — and I’ve noticed an incredible difference in my mood and outlook. Try them yourself:
+                                        On a scale of 1–10, how happy are you with your life today?</p>
+                                    <p>Don’t dwell on the question too much. You don’t need to provide a list of reasons why — just give a number based on your gut feeling.
+                                        If your answer is 10, congratulations. There’s no need for you to be here.</p>
+                                    <p>If it’s anything less than 10, here comes the next part: Take whatever number you came up with and add one.
+                                        Now ask yourself another question:</p>
+                                    <blockquote>
+                                        Why just add one? Because it’s achievable. If you add more than that Cool Pastry
+                                    </blockquote>
+
+                                    <p>The wording is important. The question “What would be different?” sends your brain on a mission to search for small, achievable changes you can implement to make your life just a little bit better the next day. Maybe you’ll decide to get outdoors for 20 minutes, or call an old friend for a quick catch-up session.
+                                        Do this consistently over the weeks and months and those changes will start to add up.</p>
+
+                                    <img src="../assets/img/blog-detail-middle.jpg" alt="image" class="mb-50 mt-50">
+
+                                    <p>
+                                        The wording is important. The question “What would be different?” sends your brain on a mission to search for small, achievable changes you can implement to make your life just a little bit better the next day. Maybe you’ll decide to get outdoors for 20 minutes, or call an old friend for a quick catch-up session. Do this consistently over the weeks and months and those changes will start to add up.
+                                        Why just add one? Because it’s achievable.
+                                    </p>
+                                    <p>If you add more than that — if you try to go from a 3 to a 9, for example — you’ll probably feel anxious about the huge undertaking and decide to put it off for later.
+                                    </p>
+
+                                    <!-- <ul class="category">
+                                    <li><span>Tags:</span></li>
+                                    <li><nuxt-link to="/">Business</nuxt-link></li>
+                                    <li><nuxt-link to="/">Sell</nuxt-link></li>
+                                    <li><nuxt-link to="/">Tips</nuxt-link></li>
+                                    <li><nuxt-link to="/">Design</nuxt-link></li>
+                                </ul> -->
+                                </div>
+                            </div>
+                            <div class="col-md-2">&nbsp;</div>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </section>
+</div>
+</template>
---
+++ pages/blog-one.vue
@@ -0,0 +1,153 @@
+<template>
+<div>
+    <!-- <div class="page-title-area">
+            <div class="container">
+                <ul>
+                    <li><nuxt-link to="/">Home</nuxt-link></li>
+                    <li>Blog</li>
+                </ul>
+            </div>
+        </div> -->
+
+    <section class="news-area">
+        <div class="container" style="margin-top:90px;">
+            <div class="row">
+                <div class="col-lg-5 mb-70">
+                    <div class="banner-content">
+                    </div>
+                    <h1 class="blogTitle">Blog</h1>
+
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-lg-5">
+                    <img src="../assets/img/about/about-first.jpg" class="img-fluid">
+                </div>
+                <div class="col-lg-7">
+                    <div class="post mt-60">
+                        <p class="futured">Feautured</p>
+                        <h1 class="blogTitle">About Cool Pastry</h1>
+                        <p class="text">
+                            Our olive oil is a celebration of the creativity of chefs and food fans the world over. We wouldn’t be here without them, so this page is all about the great work they do. In other words, these stories are about you.
+                        </p>
+                        <ul class="list-inline justify-content-center">
+                            <li class="list-inline-item"><img src="~/assets/img/group-10.png" class="Group-10"></li>
+                                    <li class="list-inline-item">
+                                        <nuxt-link to="/blog-details">Explore more</nuxt-link>
+                                    </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="news-area ptb-60">
+        <div class="container">
+            <div class="row">
+
+                <div class="col-lg-4 col-md-6" v-for="news in allNews" :key="news.id">
+                    <div class="single-news-post">
+                        <div class="news-image">
+                            <!-- <nuxt-link :to="post.link"> -->
+                            <img :src="`http://august.webertela.online/backend/web/images/store/${news.filePath}`" />
+                            <!-- </nuxt-link> -->
+                        </div>
+
+                        <div class="news-content text-center">
+                            <h3>
+                                <!-- <nuxt-link :to="post.link">{{post.title}}</nuxt-link> -->
+                                {{news.title}}
+                            </h3>
+                            <!-- <span class="author">By <a href="#">{{post.author}}</a></span> -->
+                            <!-- <p>{{post.post}}</p> -->
+                            <div>
+                                <ul class="list-inline justify-content-center">
+                                    <li class="list-inline-item readMore">&nbsp;</li>
+                                    <li class="list-inline-item">
+                                        <nuxt-link to="/blog-details">Explore more</nuxt-link>
+                                    </li>
+                                </ul>
+                            </div>
+                            <div>
+
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </section>
+</div>
+</template>
+
+<style scoped>
+
+
+.blogTitle
+{
+  font-family: 'Playfair Display SC', serif;
+  font-size: 41px;
+  font-weight: normal;
+  font-stretch: normal;
+  font-style: normal;
+  line-height: normal;
+  letter-spacing: normal;
+  color: #161e17
+}
+.futured{
+    font-family:'FiraGO', serif;
+  font-size: 16px;
+  font-weight: normal;
+  font-stretch: normal;
+  font-style: normal;
+  line-height: 1.63;
+  letter-spacing: normal;
+  color: #131415;
+}
+.post p {
+    font-family:'FiraGO', serif;
+  font-size: 16px;
+  font-weight: 200;
+  font-stretch: normal;
+  font-style: normal;
+  line-height: 1.63;
+  letter-spacing: normal;
+  color: #131415;
+}
+
+</style>
+
+<script>
+import axios from 'axios';
+export default {
+    data() {
+        return {
+            posts: [],
+            allNews: [],
+        }
+    },
+    mounted() {
+        const TOKEN = 'RiG7zh-dadLHoih5AeXXzmEbaXvWbHPS';
+
+        // var bodyFormData = new FormData();
+        // bodyFormData.set("branch", this.branch);
+        // bodyFormData.set("status_key", this.status);
+
+        axios.request({
+                method: "post",
+                url: "http://august.webertela.online/rest/web/index.php?r=v1/news/list",
+                headers: {
+                    Authorization: "Bearer " + TOKEN,
+                },
+                // data: bodyFormData,
+            })
+            .then((response) => {
+                console.log('News Response: ', response);
+                this.allNews = response.data;
+            });
+    },
+}
+</script>
